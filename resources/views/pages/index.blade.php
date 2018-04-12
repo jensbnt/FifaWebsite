ae3a734c83be011e2a4a5eb5ad24b2dbe42a686d
Jens Beernaert <jens_beernaert@hotmail.com>
2018-04-12 16:27:13 +0200

    v1.6.2 - Welcome screen

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -14,24 +14,38 @@
         @else
             <div class="row">
                 <div class="col-md">
-                    <h1>Welcome</h1>
-                    <p>Jelle can't work with laravel or lavander.</p>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h1>Welcome</h1>
+                            <p>Jelle can't work with laravel or lavander.</p>
+                            <p><a href="{{ route('players.index') }}">Players</a></p>
+                            <p><a href="{{ route('teams.index') }}">Teams</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-md">
-                    <h1>Sections</h1>
-                    <p><a href="{{ route('players.index') }}">Players</a></p>
-                    <p><a href="{{ route('teams.index') }}">Teams</a></p>
+                <div class="col-md-6">
+                    <ul class="list-group mb-3">
+                        <li class="list-group-item"><h1>Recent Updates</h1></li>
+                        <li class="list-group-item"><b>v1.6</b> - Added player graphics (picture, club and nation)</li>
+                        <li class="list-group-item"><b>v1.5</b> - Global player stats</li>
+                        <li class="list-group-item"><b>v1.4</b> - Edit player's games, goals and assists</li>
+                        <li class="list-group-item"><b>v1.3</b> - Add custom players</li>
+                        <li class="list-group-item"><b>v1.2</b> - Add a team description and edit teams</li>
+                        <li class="list-group-item"><b>v1.1</b> - User logins</li>
+                        <li class="list-group-item"><b>v1.0</b> - Players, teams and team players</li>
+                        <li class="list-group-item"><b>v0 and lower</b> - Design & testing</li>
+                    </ul>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md">
-                    <h1>To Do</h1>
-                    <p>Nations and club db</p>
-                    <p>Player sort</p>
-                    <p>User binded teams & teamplayers</p>
-                    <p>Admin sections & accounts</p>
+                <div class="col-md-6">
+                    <ul class="list-group mb-3">
+                        <li class="list-group-item"><h1>Future Updates</h1></li>
+                        <li class="list-group-item">Nations and club database</li>
+                        <li class="list-group-item">Player sort</li>
+                        <li class="list-group-item">User owned teams and team players</li>
+                        <li class="list-group-item">Admin sections and accounts</li>
+                    </ul>
                 </div>
             </div>
         @endif
